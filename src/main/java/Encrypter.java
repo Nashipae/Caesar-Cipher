82f034e0937e9763e728963e5a32074425c879b9
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-18 00:51:20 +0300

    Generate README file.

--- src/main/java/Encrypter.java
+++ src/main/java/Encrypter.java
@@ -41,9 +41,10 @@ public class Encrypter {
 
 
     public static String decrypt(String plaintext, int shift) {
-        if (shift > 26) {
+        if (shift > 26){
             shift = shift % 26;
-        } else if (shift < 0) {
+        }
+        else if (shift < 0) {
             shift = (shift % 26) + 26;
         }
 
