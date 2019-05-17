a45e2fc753696bfcb2996e4480a97b2c1cc738a7
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-17 22:33:12 +0300

    Add decrypter.

--- src/main/java/Decrypter.java
+++ src/main/java/Decrypter.java
@@ -1,49 +1,51 @@
-public class Decrypter {
-
-    public String decrypt(String plaintext, int shift) {
-        if (shift > 26) {
-            shift = shift % 26;
-        } else if (shift < 0) {
-            shift = (shift % 26) + 26;
-        }
-
-        String cipherText = "";
-        int length = plaintext.length();
-        for (int i = 0; i < length; i++) {
-            char ch = plaintext.charAt(i);
-            if (Character.isLetter(ch)) {
-                if (Character.isLowerCase(ch)) {
-                    char c = (char) (ch + shift);
-                    if (c > 'z') {
-                        cipherText += (char) (ch - (26 - shift));
-                    } else {
-                        cipherText += c;
-                    }
-
-                } else if (Character.isUpperCase(ch)) {
-                    char c = (char) (ch + shift);
-                    if (c > 'Z') {
-                        cipherText += (char) (ch - (26 - shift));
-                    } else {
-                        cipherText += c;
-                    }
-                }
-
-            } else {
-                cipherText += ch;
-            }
-        }
-        return cipherText;
-
-    }
-
-
-
-
+//public class Decrypter {
+//
+//    public static String decrypt(String plaintext, int shift) {
+//        if (shift > 26) {
+//            shift = shift % 26;
+//        } else if (shift < 0) {
+//            shift = (shift % 26) + 26;
+//        }
+//
+//        String cipherText = "";
+//        int length = plaintext.length();
+//        for (int i = 0; i < length; i++) {
+//            char ch = plaintext.charAt(i);
+//            if (Character.isLetter(ch)) {
+//                if (Character.isLowerCase(ch)) {
+//                    char c = (char) (ch - shift);
+//                    if (c < 'a') {
+//                        cipherText += (char) (ch + (26 - shift));
+//                    } else {
+//                        cipherText += c;
+//                    }
+//
+//                } else if (Character.isUpperCase(ch)) {
+//                    char c = (char) (ch - shift);
+//                    if (c < 'A') {
+//                        cipherText += (char) (ch + (26 - shift));
+//                    } else {
+//                        cipherText += c;
+//                    }
+//                }
+//
+//            } else {
+//                cipherText += ch;
+//            }
+//        }
+//        return cipherText;
+//
+//    }
+//
+//
+//
 //public static void main(String[] args){
-//        String text = "message";
+//       String text = "message";
 //        String cipher = encrypt(text, 5);
 //        System.out.println(cipher);
+//        String decrypted = decrypt(cipher,5);
+//        System.out.println(decrypted);
+//
 //}
-
-}
+//
+//}
---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public void main(String[] args) {
+
+//        Get user input
+        Scanner requestText = new Scanner(System.in);
+        System.out.println(" What is the text that you would want encrypted : ");
+        String userInputtedText = requestText.nextLine();
+
+
+//        Get user input on shift value
+        System.out.println(" Enter the value by which each character in the plaintext message gets shifted : ");
+//        int shift = requestText.nextInt();
+
+//        Encrypter myEncrypter = new Encrypter();
+//        String cipher = myEncrypter.encrypt(requestText, shift);
+//        System.out.println("Your encrypted text is : " + cipher);
+//
+//        Decrypter mydecrypter = new Decrypter();
+//        String cipher = myDecrypter.decrypt(cipher, shift);
+//        System.out.println("Your encrypted text is : " + cipher);
+//
+//    }
+//
+//
+//    }
+    }
+
+}
