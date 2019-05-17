d315575c3e583b4bb8b92f45ebec5db7bd8fc04f
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-18 01:26:22 +0300

    add files

---
+++ src/test/java/EncrypterTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EncrypterTest {
+
+
+
+    @Test
+    public void encryptTest() {
+        Encrypter text = new Encrypter();
+        String result = "bc";
+        assertEquals(result, text.encrypt("ab", 1));
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.io.Console;
+
+public class MainTest {
+
+   @Test
+   public void MainTest() {
+       Console myConsole = System.console();
+
+        System.out.println("What is your favorite color?");
+    }
+}
