419f02b67e70f23370705057893a3e25a5f60e32
Naomi Wariara <nwariara@safaricom.co.ke>
2019-05-17 22:01:06 +0100

    edit filename

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,13 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.Console;
 
 public class MainTest {
 
-    @Test
-    public void main() {
+   @Test
+   public void MainTest() {
+       Console myConsole = System.console();
+
+        System.out.println("What is your favorite color?");
     }
 }
